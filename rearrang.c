0c96b5d0c6537e15536824d7c89e9b0018f77261
Xiaozhuoer <1225691169@qq.com>
2020-12-15 21:28:27 +0800

    commit

--- rearrang.c
+++ rearrang.c
@@ -14,14 +14,13 @@
 #define MAX_INPUT 1000 /* 每个输入行的最大长度 */
 
 int read_column_numbers(int columnsp[], int max );
-void rearrange( char *output, char const *input,
-                int n_columns, int const columns[] );
+void rearrange( char *output, char const *input, int n_columns, int const columns[] );
 
 int main( void )
 {
-    int n_columns; /* 进行处理的列标号 */
-    int columns[MAX_COLS]; /* 需要处理的列数 */
-    char input[MAX_INPUT]; /* 容纳输入行的数组 */
+    int n_columns;  /* 进行处理的列标号 */
+    int columns[MAX_COLS];  /* 需要处理的列数 */
+    char input[MAX_INPUT];  /* 容纳输入行的数组 */
     char output[MAX_INPUT]; /* 容纳输出行的数组 */
 
     /*
@@ -72,5 +71,40 @@ int read_column_numbers( int columns[], int max)
 }
 
 /*
-** 处理输入行
+** 处理输入行，将指定列的字符连接在一起，输出行以NUL结尾。
 */
+void rearrange( char *output, char const *input, int n_columns, int const columns[] )
+{
+    int col;    /* columns数组的下标 */
+    int output_col; /* 输出列计数器 */
+    int len;    /* 输入行的长度 */
+
+    len = strlen ( input );
+    output_col = 0;
+
+    /*
+    ** 处理每对列标号。
+    */
+    for ( col = 0; col < n_columns; col += 2 ){
+        int nchars = columns[col + 1] - columns[col] + 1;
+
+        /*
+        ** 如果输入行结束或输出行数组已满，就结束任务
+        */
+        if( columns[col] >= len || output_col == MAX_INPUT - 1)
+            break;
+        
+        /*
+        ** 如果输出行数据空间不够，只复制可以容纳的数据。
+        */
+        if( output_col + nchars > MAX_INPUT - 1 )
+            nchars = MAX_INPUT - output_col - 1;
+
+        /*
+        ** 复制相关的数据。
+        */
+       strncpy( output + output_col, input + columns[col], nchars );
+       output_col += nchars;
+    } 
+    output[output_col] = '\0';
+}
