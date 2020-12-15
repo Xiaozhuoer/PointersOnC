476c7d00380f1025c70ef462cbcf1a25123fd93e
Xiaozhuoer <1225691169@qq.com>
2020-12-15 21:07:20 +0800

    commit

--- rearrang.c
+++ rearrang.c
@@ -7,4 +7,70 @@
 ** 例如，0 3 10 12 -1 表示第0列到第3列，第10列到第12列的内容将被打印。
 */
 
-1
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_COLS 20 /* 能够处理的最大列号 */
+#define MAX_INPUT 1000 /* 每个输入行的最大长度 */
+
+int read_column_numbers(int columnsp[], int max );
+void rearrange( char *output, char const *input,
+                int n_columns, int const columns[] );
+
+int main( void )
+{
+    int n_columns; /* 进行处理的列标号 */
+    int columns[MAX_COLS]; /* 需要处理的列数 */
+    char input[MAX_INPUT]; /* 容纳输入行的数组 */
+    char output[MAX_INPUT]; /* 容纳输出行的数组 */
+
+    /*
+    ** 读取该串列标号
+    */
+    n_columns = read_column_numbers( columns, MAX_COLS );
+
+    /*
+    ** 读取、处理和打印剩余的输入行
+    */
+    while( gets( input ) != NULL ){
+        printf( "Original input : %s\n", input );
+        rearrange ( output , input , n_columns , columns );
+        printf( "Rearrange line: %s\n", output );
+    }
+
+    return EXIT_SUCCESS;
+}
+
+/*
+** 读取列标号，如果超出规定范围则不予理会
+*/
+int read_column_numbers( int columns[], int max)
+{
+    int num = 0;
+    int ch;
+    
+    /*
+    ** 取得列标号，如果所读取的数小于0则停止。
+    */
+    while ( num < max && scanf( "%d", &columns[num] ) == 1 && columns[num] >= 0)
+        num += 1;
+    
+    /*
+    ** 确认已经读取的标号为偶数个，因为它们是以对的形式出现的。
+    */
+    if ( num % 2 != 0 ){
+        puts( "Last column number is not paired." );
+        exit( EXIT_FAILURE );
+    }
+
+    /*
+    ** 丢弃该行中包含最后一个数字的那部分内容
+    */
+    while( (ch = getchar()) != EOF && ch != '\n' )
+        ;
+    return num;
+}
+
+/*
+** 处理输入行
+*/
